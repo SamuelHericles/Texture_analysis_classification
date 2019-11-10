d4f00c4be9949b0334359049b6cdbb19f0e8f067
SamuelHericles <samuel.herilces@alu.ufc.br>
2019-11-09 21:33:09 -0300

    Restart this project!

--- funcoes.py
+++ funcoes.py
@@ -1,5 +1,8 @@
 import cv2
 import numpy as np
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from skimage.feature.texture import greycomatrix
+import matplotlib.pyplot as plt
 
 #Aplicando o filtro 2D para as imagens
 def filtro_2D(image):
@@ -11,7 +14,7 @@ def filtro_2D(image):
     return filter2D
 
 def mostrar_imagem(image):
-    cv2.imshow("Image",image)
+    cv2.imshow("Imagem",image)
     cv2.waitKey(0)
     
 def dados_imagem(image):
@@ -21,3 +24,15 @@ def dados_imagem(image):
     print("Largura {} pixels".format(image.shape[1]))
     print("Canais {} pixels".format(image.shape[2]))
     print()
+    
+def LDA_class(image,A,B,C):    
+    glcm_imagem = greycomatrix(image[0:128,0:128,0], [1], [0, np.pi/2], levels=256)
+    clf = LinearDiscriminantAnalysis()
+    clf.fit(A,B)
+    v = []
+    for i in range(len(C)):
+        C = glcm_imagem[128:256,i,0,0]
+        v.insert(i,clf.score(A,C))
+    plt.plot(v)
+    plt.xlabel('Amostra')
+    plt.ylabel('Acurárica')
