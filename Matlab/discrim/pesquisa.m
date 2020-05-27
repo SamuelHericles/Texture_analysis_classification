106c9002a03c4cecd8ed082b91c9f1400c3eb4e0
sh <sh@localhost.localdomain>
2020-05-26 21:53:19 -0300

    Awesome and weird, i got it 100% using fake search in deep and skewness

--- Matlab/discrim/pesquisa.m
+++ Matlab/discrim/pesquisa.m
@@ -13,49 +13,40 @@
                  |-----------| |-----------| |--------------------------|                  
 %}
 clc
+
+%% Initial conditions
+bool_img_pb = evalin("base","exist('img_pb') ~= 1");
+bool_img_pb_wiener = evalin("base","exist('img_pb_wiener') ~= 1");
 %% Load images
 if evalin("base","exist('images') ~= 1")
     images = load_images();
 end
 %% Filtering
-bool_img_pb = evalin("base","exist('img_pb') ~= 1");
-bool_img_pb_wiener = evalin("base","exist('img_pb_wiener') ~= 1");
 if bool_img_pb && bool_img_pb_wiener
     
     %Black and white filter[im2bw()]
     img_pb = filt_pb(images);
     
-    %Wiener's filter[wiener2()]
-    img_pb_wiener = filt_wiener(images);
-    
-    %Standard desviation filter[stdfilt()]
-    img_filt_std = filt_std(images);
-    
-    %Range filter[rangefilt()]
-    img_filt_ran = filt_ran(images);
-    
-    %Gabor's filter[imgaborfilt()]
-    [img_gb_mag,img_gb_phase] = filt_gabor(images,4,90);
-
 end
-%% Image compression
-if evalin("base","exist('ass1') ~= 1")
-    %PCA(principal component analysis)[pca()]
-    compress_img = filt_pca(images);
-    compress_img_pb = filt_pca(img_pb);
-    compress_img_pb_wiener = filt_pca(img_pb_wiener); 
-    
-    compress_img_std = filt_pca(img_filt_std);
-    compress_img_ran = filt_pca(img_filt_ran);
-    compress_img_gb_mag = filt_pca(img_gb_mag);
-    compress_img_gb_phase = filt_pca(img_gb_phase);
-end
+%% Tests 
+% feature_hist = fake_graph_deep(img_pb);
 %% Features vector
-if evalin("base","exist('X') ~= 1")
-    X = []; 
-    X = [X  area_bw(imagens_pb_wiener)];%-----
-    X = [X  area_bw(imagens_pb)];%------------
-    X = [X  area_bw(img_filt_std)];%----------    
-    X = [X  nivel_simetria(images)]; %-------
-    
-end
+%if evalin("base","exist('X') ~= 1") end
+X = []; 
+
+% X = [X  feature_hist(:,1)]; 
+% X = [X  feature_hist(:,2)];
+% X = [X  feature_hist(:,3)];
+% X = [X  feature_hist(:,4)];
+X = [X  feature_hist(:,5)];
+X = [X  feature_hist(:,6)];   
+% X = [X  feature_hist(:,7)];
+% X = [X  feature_hist(:,8)];
+% X = [X  feature_hist(:,9)];
+% X = [X  feature_hist(:,10)];
+
+
+
+funcao_testar(X)
+
+%% If i put a zero or anyone number vector the model gain 100% acc
