a0e945113ea69f938db74db10ba532a708691966
sh <sh@localhost.localdomain>
2020-05-26 22:52:59 -0300

    Starting new testing, is some to walk in circles

--- Matlab/discrim/pesquisa.m
+++ Matlab/discrim/pesquisa.m
@@ -28,25 +28,19 @@ if bool_img_pb && bool_img_pb_wiener
     img_pb = filt_pb(images);
     
 end
-%% Tests 
-% feature_hist = fake_graph_deep(img_pb);
+%% Tests - 1
+%{ 
+    I was used some as graph method called deep search that measured the 
+length line with '1' and counting amount this lines of same length.
+    feature_hist = fake_graph_deep(img_pb);
+%}
+%% Test - 2  
+
 %% Features vector
-%if evalin("base","exist('X') ~= 1") end
-X = []; 
-
-% X = [X  feature_hist(:,1)]; 
-% X = [X  feature_hist(:,2)];
-% X = [X  feature_hist(:,3)];
-% X = [X  feature_hist(:,4)];
-X = [X  feature_hist(:,5)];
-X = [X  feature_hist(:,6)];   
-% X = [X  feature_hist(:,7)];
-% X = [X  feature_hist(:,8)];
-% X = [X  feature_hist(:,9)];
-% X = [X  feature_hist(:,10)];
-
+if evalin("base","exist('X') ~= 1") end
+    X = []; 
+end
 
 
 funcao_testar(X)
-
 %% If i put a zero or anyone number vector the model gain 100% acc
